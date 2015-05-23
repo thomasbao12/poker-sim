7c11416b1d5df8c9122f3fc96b8841996df60087
duarteguilherme <gjardimduarte@gmail.com>
2015-05-23 14:56:26 -0300

    Fix the Dictionary of Hands

--- simulator.py
+++ simulator.py
@@ -13,16 +13,16 @@ dict_cards = dict(zip(cards, range(8,60)))
 
 
 dict_hands = {
-    (): ["straight flush"],
-    (): ["four"],
-    (): ["full-house"],
-    (): ["flush"],
-    (): ["flush"],
-    (): ["straight"],
-    (): ["three"],
-    (): ["two pairs"],
-    (): ["pair"],
-    (): ["high card"]
+    (1,1,5,1): ["straight flush",9],
+    (0,0,2,4): ["four",8],
+    (0,0,2,3): ["full-house",7],
+    (0,1,4,2): ["flush",6],
+    (0,1,5,1): ["flush",6],
+    (1,0,5,1): ["straight",5],
+    (0,0,3,3): ["three",4],
+    (0,0,3,2): ["two pairs",3],
+    (0,0,4,2): ["pair",2],
+    (0,0,5,1): ["high card",1]
 }
 
 
